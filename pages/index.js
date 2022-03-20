9d143595cebbdec2d9434bade2afc7f68ca46186
Satwinder Singh <satwindersinghsaini59@gmail.com>
2022-03-20 17:34:32 -0400

    import fix

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
-import Date from "../components/date";
+import Date from "../Components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
